835c7e64abfcf367ad70524080747a088b889bdc
Gulrez Alam <egulrezalam@gmail.com>
2023-12-03 23:41:53 +0530

    Refactor Redux actions and API calls

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -1,118 +1,133 @@
-import axios from "axios"
-import { toast } from "react-toastify"
-import Updateuser from "../Component/Updateuser"
-import { ADD_USER, DELETE_USER, FAIL_REQUEST, GET_USER_LIST, GET_USER_OBJ, MAKE_REQUEST, UPDATE_USER } from "./ActionType"
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Updateuser } from "../Component/Updateuser";
+import {
+  ADD_USER,
+  DELETE_USER,
+  FAIL_REQUEST,
+  GET_USER_LIST,
+  GET_USER_OBJ,
+  MAKE_REQUEST,
+  UPDATE_USER,
+} from "./ActionType";
 
-export const makeRequest=()=>{
-    return{
-        type:MAKE_REQUEST
-    }
-}
-export const failRequest=(err)=>{
-    return{
-        type:FAIL_REQUEST,
-        payload:err
-    }
-}
-export const geUserList=(data)=>{
-    return{
-        type:GET_USER_LIST,
-        payload:data
-    }
-}
-export const deleteUser=()=>{
-    return{
-        type:DELETE_USER
-    }
-}
-export const addUser=()=>{
-    return{
-        type:ADD_USER
-    }
-}
-export const updateUser=()=>{
-    return{
-        type:UPDATE_USER
-    }
-}
-export const getUserObj=(data)=>{
-    return{
-        type:GET_USER_OBJ,
-        payload:data
-    }
-}
+const API_BASE_URL = "https://hirequotient.onrender.com"; 
 
+export const makeRequest = () => {
+  return {
+    type: MAKE_REQUEST,
+  };
+};
 
+export const failRequest = (err) => {
+  return {
+    type: FAIL_REQUEST,
+    payload: err,
+  };
+};
 
-export const FetchUserList=()=>{
-    return (dispatch)=>{
-      dispatch(makeRequest());
-      //setTimeout(() => {
-        axios.get('http://localhost:8000/user').then(res=>{
-            const userlist=res.data;
-            dispatch(geUserList(userlist));
-          }).catch(err=>{
-            dispatch(failRequest(err.message))
-          })
-     // }, 2000);
-     
-    }
-}
+export const geUserList = (data) => {
+  return {
+    type: GET_USER_LIST,
+    payload: data,
+  };
+};
 
-export const Removeuser=(code)=>{
-    return (dispatch)=>{
-      dispatch(makeRequest());
-      //setTimeout(() => {
-        axios.delete('http://localhost:8000/user/'+code).then(res=>{
-            dispatch(deleteUser());
-          }).catch(err=>{
-            dispatch(failRequest(err.message))
-          })
-     // }, 2000);
-     
-    }
-}
+export const deleteUser = () => {
+  return {
+    type: DELETE_USER,
+  };
+};
 
-export const FunctionAddUser=(data)=>{
-    return (dispatch)=>{
-      dispatch(makeRequest());
-      //setTimeout(() => {
-        axios.post('http://localhost:8000/user',data).then(res=>{
-            dispatch(addUser());
-            toast.success('User Added successfully.')
-          }).catch(err=>{
-            dispatch(failRequest(err.message))
-          })
-     // }, 2000);
-     
-    }
-}
+export const addUser = () => {
+  return {
+    type: ADD_USER,
+  };
+};
 
-export const FunctionUpdateUser=(data,code)=>{
-    return (dispatch)=>{
-      dispatch(makeRequest());
-      //setTimeout(() => {
-        axios.put('http://localhost:8000/user/'+code,data).then(res=>{
-            dispatch(updateUser());
-            toast.success('User Updated successfully.')
-          }).catch(err=>{
-            dispatch(failRequest(err.message))
-          })
-     // }, 2000);
-     
-    }
-}
-export const FetchUserObj=(code)=>{
-    return (dispatch)=>{
-      dispatch(makeRequest());
-      //setTimeout(() => {
-        axios.get('http://localhost:8000/user/'+code).then(res=>{
-            const userlist=res.data;
-            dispatch(getUserObj(userlist));
-          }).catch(err=>{
-            dispatch(failRequest(err.message))
-          })
-     // }, 2000);
-     
-    }
-}
+export const updateUser = () => {
+  return {
+    type: UPDATE_USER,
+  };
+};
+
+export const getUserObj = (data) => {
+  return {
+    type: GET_USER_OBJ,
+    payload: data,
+  };
+};
+
+export const FetchUserList = () => {
+  return (dispatch) => {
+    dispatch(makeRequest());
+    axios
+      .get(`${API_BASE_URL}/user`)
+      .then((res) => {
+        const userlist = res.data;
+        dispatch(geUserList(userlist));
+      })
+      .catch((err) => {
+        dispatch(failRequest(err.message));
+      });
+  };
+};
+
+export const Removeuser = (code) => {
+  return (dispatch) => {
+    dispatch(makeRequest());
+    axios
+      .delete(`${API_BASE_URL}/user/${code}`)
+      .then((res) => {
+        dispatch(deleteUser());
+      })
+      .catch((err) => {
+        dispatch(failRequest(err.message));
+      });
+  };
+};
+
+export const FunctionAddUser = (data) => {
+  return (dispatch) => {
+    dispatch(makeRequest());
+    axios
+      .post(`${API_BASE_URL}/user`, data)
+      .then((res) => {
+        dispatch(addUser());
+        toast.success("User Added successfully.");
+      })
+      .catch((err) => {
+        dispatch(failRequest(err.message));
+      });
+  };
+};
+
+export const FunctionUpdateUser = (data, code) => {
+  return (dispatch) => {
+    dispatch(makeRequest());
+    axios
+      .put(`${API_BASE_URL}/user/${code}`, data)
+      .then((res) => {
+        dispatch(updateUser());
+        toast.success("User Updated successfully.");
+      })
+      .catch((err) => {
+        dispatch(failRequest(err.message));
+      });
+  };
+};
+
+export const FetchUserObj = (code) => {
+  return (dispatch) => {
+    dispatch(makeRequest());
+    axios
+      .get(`${API_BASE_URL}/user/${code}`)
+      .then((res) => {
+        const userlist = res.data;
+        dispatch(getUserObj(userlist));
+      })
+      .catch((err) => {
+        dispatch(failRequest(err.message));
+      });
+  };
+};
